586b6b0edb52814b27b1794881f91f22bfed4310
qkwmapel <otxoid@gmail.com>
2024-03-05 19:33:03 +0900

    test

--- calculator1.playground/Contents.swift
+++ calculator1.playground/Contents.swift
@@ -21,8 +21,8 @@ class Calculator {
 let calculator = Calculator()
 let addResult = calculator.calculate(oper: "+", firstNumber: 1, secondNumber: 2)
 let subtractResult = calculator.calculate(oper: "-", firstNumber: 9, secondNumber: 2)// 뺄셈 연산
-let multiplyResult = calculator.calculate(oper: "*", firstNumber: 4, secondNumber: 2)// 곱셈 연산
-let divideResult = calculator.calculate(oper: "/", firstNumber: 6, secondNumber: 2)// 나눗셈 연산
+let multiplyResult = calculator.calculate(oper: "*", firstNumber: 6, secondNumber: 2)// 곱셈 연산
+let divideResult = calculator.calculate(oper: "/", firstNumber: 8, secondNumber: 2)// 나눗셈 연산
 
 print("addResult : \(addResult)")
 print("subtractResult : \(subtractResult)")
