11b742d4c70c06a178e4a7788f5181e1ac226edc
qkwmapel <otxoid@gmail.com>
2024-03-05 18:24:48 +0900

    calculator step 1

---
+++ calculator1.playground/Contents.swift
@@ -0,0 +1,30 @@
+import UIKit
+
+class Calculator {
+    func calculate(oper: String, firstNumber: Int, secondNumber: Int ) -> Double {
+        switch oper {
+        case "+" :
+            return Double(firstNumber + secondNumber)
+        case "-" :
+            return Double(firstNumber - secondNumber)
+        case "*" :
+            return Double(firstNumber * secondNumber)
+        case "/" :
+            return Double(firstNumber / secondNumber)
+        default:
+            return 0
+        }
+        // 정확한 operator가 전달되지 않을 시 0을 리턴(nil을 리턴해도 됨)
+    }
+}
+
+let calculator = Calculator()
+let addResult = calculator.calculate(oper: "+", firstNumber: 1, secondNumber: 2)
+let subtractResult = calculator.calculate(oper: "-", firstNumber: 9, secondNumber: 2)// 뺄셈 연산
+let multiplyResult = calculator.calculate(oper: "*", firstNumber: 4, secondNumber: 2)// 곱셈 연산
+let divideResult = calculator.calculate(oper: "/", firstNumber: 6, secondNumber: 2)// 나눗셈 연산
+
+print("addResult : \(addResult)")
+print("subtractResult : \(subtractResult)")
+print("multiplyResult : \(multiplyResult)")
+print("divideResult : \(divideResult)")
